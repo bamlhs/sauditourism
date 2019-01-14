60bbdefb4dac9ce5a6eb9a6c15ccdbc79cef9041
Abdullah Bamelhes <bamlhs@gmail.com>
2019-01-14 21:22:20 +0300

    styling customized buttons

--- sauditourism/src/components/CustomizedButton.js
+++ sauditourism/src/components/CustomizedButton.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import {Button} from 'react-native-elements';
+import { Button } from 'react-native-elements';
 
 const CustomizedButton = ({
-    title,onPress,
+    title, onPress,
 }) => (
-    <View>
-        <Button title={title} 
-            onPress={onPress}
+        <View>
+            <Button
+                textStyle={{
+                    fontFamily: "HelveticaNeue",
+                    fontSize: 18,
+                    fontWeight: "bold",
+                    fontStyle: "normal",
+                    lineHeight: 27,
+                    letterSpacing: 0,
+                    textAlign: "left",
+                    color: "#707070"
+                }}
+                buttonStyle={{
+                    backgroundColor: '#ffff',
+                    borderColor: 'rgb(107,209,61)',
+                    borderWidth: 2,
+                    borderRadius: 30,
+                }}
+                title={title}
+                onPress={onPress}
 
             />
-    </View>
-);
+        </View>
+    );
 
 export default CustomizedButton;
--- sauditourism/src/screens/DashBoardScreen/DashBoardScreen.js
+++ sauditourism/src/screens/DashBoardScreen/DashBoardScreen.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text, Image, Dimensions} from 'react-native';
+import { View, Text, ImageBackground, Dimensions } from 'react-native';
 import CustomizedButton from '../../components/CustomizedButton';
-const { width, height} = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 
 export default class DashBoardScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
 
-  render() {
-    return (
-      <View>
-        <Image
-        source={require('../../images/saudi-tourism.jpg')} 
-         style={{width: width, height: height}} 
-         resizeMode="cover"
-        >
-        <CustomizedButton title="Hello" />
-        </Image>
-  
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <ImageBackground
+                    source={require('../../images/saudi-tourism.jpg')}
+                    style={{ width: width, height: height }}
+                    resizeMode="cover"
+                >
+                    <View style={{ flex: 1 }}>
+                        <View style={{ flex: 7 }}></View>
+                        <View style={{ flex: 1, flexDirection: 'row', }}>
+                            <View style={{ flex: 1, }}>
+                                <CustomizedButton title="Show Places" />
+                            </View>
+                            <View style={{ flex: 1, }}>
+                                <CustomizedButton title="Add Place" />
+                            </View>
+                        </View>
+                    </View>
+                </ImageBackground>
+
+            </View>
+        );
+    }
 }
